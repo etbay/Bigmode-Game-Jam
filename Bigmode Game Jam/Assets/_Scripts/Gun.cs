4a9d9310d8c993d06ba0b575c5dfc493b90f87b0
VortexCoreCoding <logan.spinali@wsu.edu>
2026-01-31 14:13:18 -0800

    Clicking shoots ur gun

--- Bigmode Game Jam/Assets/_Scripts/Gun.cs
+++ Bigmode Game Jam/Assets/_Scripts/Gun.cs
@@ -3,19 +3,10 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     public Camera cam;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        if (true)
-        {
-            Shoot();
-        }
+        
     }
 
     void Shoot()
@@ -23,10 +14,6 @@ public class Gun : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit))
         {
-            // if (hit.collider.gameObject.GetComponent<Destructible>() != null)
-            // {
-            //     Debug.Log(hit.transform.name);
-            // }
             hit.collider.gameObject.GetComponent<Destructible>()?.Kill();
             Debug.DrawRay(cam.transform.position, cam.transform.forward, Color.red);
         }
