0b606acaaf3b81304744825435bec9aa0961f71e
VortexCoreCoding <logan.spinali@wsu.edu>
2026-01-31 22:13:20 -0800

    sfxmanager singleton and omnicient and point sfx support

--- Bigmode Game Jam/Assets/_Scripts/AudioManager.cs
+++ Bigmode Game Jam/Assets/_Scripts/AudioManager.cs
@@ -1,9 +1,11 @@
+using System.Collections;
+using System.Diagnostics;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
-    [SerializeField] private AudioSource sfxObject;
+    [SerializeField] private GameObject sfxObject;
 
     [SerializeField] private GameObject player;
 
@@ -15,24 +17,30 @@ public class AudioManager : MonoBehaviour
         }
 
     }
-    public AudioSource PlayOmnicientSoundClip(AudioClip audioclip, float vol)
+    public AudioSource PlayOmnicientSoundClip(AudioClip audioClip, float vol)
     {
-        AudioSource audioSource = Instantiate(sfxObject, player.transform.position, Quaternion.identity);
-        audioSource.clip = audioclip;
-        audioSource.volume = vol;
-        audioSource.Play();
-        float clipLength = audioSource.clip.length;
-        Destroy(audioSource, clipLength);
-        return audioSource;
+        GameObject audioSource = Instantiate(sfxObject, player.transform.position, Quaternion.identity);
+        audioSource.GetComponent<AudioSource>().clip = audioClip;
+        audioSource.GetComponent<AudioSource>().volume = vol;
+        audioSource.GetComponent<AudioSource>().Play();
+        float clipLength = audioClip.length;
+        StartCoroutine(KillAudioSource(audioSource, clipLength));
+        return audioSource.GetComponent<AudioSource>();
     }
-    public AudioSource PlaySoundClip(AudioClip audioclip, Transform spawnTransform, float vol)
+    public AudioSource PlaySoundClip(AudioClip audioClip, Transform spawnTransform, float vol)
     {        
-        AudioSource audioSource = Instantiate(sfxObject, spawnTransform.position, Quaternion.identity);
-        audioSource.clip = audioclip;
-        audioSource.volume = vol;
-        audioSource.Play();
-        float clipLength = audioSource.clip.length;
-        Destroy(audioSource, clipLength);
-        return audioSource;
+        GameObject audioSource = Instantiate(sfxObject, spawnTransform.position, Quaternion.identity);
+        audioSource.GetComponent<AudioSource>().clip = audioClip;
+        audioSource.GetComponent<AudioSource>().volume = vol;
+        audioSource.GetComponent<AudioSource>().Play();
+        float clipLength = audioClip.length;
+        StartCoroutine(KillAudioSource(audioSource, clipLength));
+        return audioSource.GetComponent<AudioSource>();
+    }
+
+    private IEnumerator KillAudioSource(GameObject target, float timeWait)
+    {
+        yield return new WaitForSeconds(timeWait);
+        Destroy(target);
     }
 }
