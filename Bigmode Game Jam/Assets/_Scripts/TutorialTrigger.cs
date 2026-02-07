bbcdf7d2f02dde514f830eb212f99f3d1fa99405
Evan Bay <116117905+etbay@users.noreply.github.com>
2026-02-07 08:51:18 -0800

    importing and polishing

--- Bigmode Game Jam/Assets/_Scripts/TutorialTrigger.cs
+++ Bigmode Game Jam/Assets/_Scripts/TutorialTrigger.cs
@@ -1,14 +1,19 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class TutorialTrigger : MonoBehaviour
 {
     [SerializeField] private string displayText;
+    [SerializeField] private TutorialDisplay canvasDisplay;
+    private bool beenDisplayed = false;
 
     void OnTriggerEnter(Collider other) // Use OnTriggerEnter2D for 2D games
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && !beenDisplayed)
         {
-            Debug.Log(displayText);
+            beenDisplayed = true;
+            canvasDisplay.Show(displayText);
         }
     }
 }
