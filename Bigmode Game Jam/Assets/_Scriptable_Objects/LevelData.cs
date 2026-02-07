6ef6d320546698a28afd96188e487272ef82959e
VortexCoreCoding <logan.spinali@wsu.edu>
2026-02-07 01:46:30 -0800

    level data change to contain new info

--- Bigmode Game Jam/Assets/_Scriptable_Objects/LevelData.cs
+++ Bigmode Game Jam/Assets/_Scriptable_Objects/LevelData.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "LevelData", menuName = "Scriptable Objects/LevelData")]
 public class LevelData : ScriptableObject
 {
+    public string sceneName;
+    public string levelName;
     public Ranking.RankRequirements requirements;
     public string nextLevel;
 }
