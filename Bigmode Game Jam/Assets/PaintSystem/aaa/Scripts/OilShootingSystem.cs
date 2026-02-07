0a4339feb560715bc1e45b095530f9e191fc7717
NomadicCat <55854868+NomadicCat@users.noreply.github.com>
2026-02-06 20:13:13 -0800

    functional oil

--- Bigmode Game Jam/Assets/PaintSystem/aaa/Scripts/OilShootingSystem.cs
+++ Bigmode Game Jam/Assets/PaintSystem/aaa/Scripts/OilShootingSystem.cs
@@ -22,44 +22,61 @@ public class OilShootingSystem : MonoBehaviour
     {
         _requestedSecondaryFire = input.SecondaryFire;
     }
+    private bool _isPlaying;
 
     void Update()
     {
+        parentController.position = splatGunNozzle.position;
+        parentController.rotation = Quaternion.LookRotation(splatGunNozzle.right);
 
-        if (_requestedSecondaryFire)
+        if (_requestedSecondaryFire && !_isPlaying)
         {
-            
-            parentController.transform.position = splatGunNozzle.position;
-            parentController.transform.rotation = Quaternion.LookRotation(splatGunNozzle.right);
-
             inkParticle.Play();
+            _isPlaying = true;
         }
-        else
+        else if (!_requestedSecondaryFire && _isPlaying)
         {
-            inkParticle.Stop();
+            inkParticle.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+            _isPlaying = false;
         }
-
-        //Vector3 angle = parentController.localEulerAngles;
-        //input.blockRotationPlayer = Input.GetMouseButton(0);
-        //bool pressing = Input.GetMouseButton(0);
-
-        //if (Input.GetMouseButton(0))
-        //{
-        //    //VisualPolish();
-        //    input.RotateToCamera(transform);
-        //}
-
-        //if (Input.GetMouseButtonDown(0))
-        //    inkParticle.Play();
-        //else if (Input.GetMouseButtonUp(0))
-        //    inkParticle.Stop();
-
-        //parentController.localEulerAngles
-        //    = new Vector3(Mathf.LerpAngle(parentController.localEulerAngles.x, pressing ? RemapCamera(freeLookCamera.m_YAxis.Value, 0, 1, -25, 25) : 0, .3f), angle.y, angle.z);
     }
+    //void Update()
+    //{
+
+    //    if (_requestedSecondaryFire)
+    //    {
+
+    //        parentController.transform.position = splatGunNozzle.position;
+    //        parentController.transform.rotation = Quaternion.LookRotation(splatGunNozzle.right);
+
+    //        inkParticle.Play();
+    //    }
+    //    else
+    //    {
+    //        inkParticle.Stop();
+    //    }
+
+    //    //Vector3 angle = parentController.localEulerAngles;
+    //    //input.blockRotationPlayer = Input.GetMouseButton(0);
+    //    //bool pressing = Input.GetMouseButton(0);
+
+    //    //if (Input.GetMouseButton(0))
+    //    //{
+    //    //    //VisualPolish();
+    //    //    input.RotateToCamera(transform);
+    //    //}
+
+    //    //if (Input.GetMouseButtonDown(0))
+    //    //    inkParticle.Play();
+    //    //else if (Input.GetMouseButtonUp(0))
+    //    //    inkParticle.Stop();
+
+    //    //parentController.localEulerAngles
+    //    //    = new Vector3(Mathf.LerpAngle(parentController.localEulerAngles.x, pressing ? RemapCamera(freeLookCamera.m_YAxis.Value, 0, 1, -25, 25) : 0, .3f), angle.y, angle.z);
+    //}
 
 
-    
+
 
     //void VisualPolish()
     //{
