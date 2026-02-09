38067b565ee034c5fe875c6b8e2f3a772056d993
DonnerTech <77709088+DonnerTech@users.noreply.github.com>
2026-02-08 20:44:55 -0800

    drain ramp

--- Bigmode Game Jam/Assets/Player/PlayerScripts/Player.cs
+++ Bigmode Game Jam/Assets/Player/PlayerScripts/Player.cs
@@ -90,7 +90,7 @@ public class Player : MonoBehaviour
                 slickValue = 1f;
             if (slickValue > maxSlick && SlickometerData.CurrentSlickDrainRate != SlickometerData.TimeslowSlickDrainRate)
             {
-                SlickometerData.CurrentSlickDrainRate = SlickometerData.OverSlickDrainRate + (slickValue - 4) * SlickometerData.OverSlickDrainRamp;
+                SlickometerData.CurrentSlickDrainRate = SlickometerData.OverSlickDrainRate + (slickValue - maxSlick) * SlickometerData.OverSlickDrainRamp;
                 Debug.Log("Overslicking!");
                 Debug.Log($"Value:{slickValue}");
             }
